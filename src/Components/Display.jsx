f6be5c1426072f577c58184102de50edb682ac92
Jhonny Varela <jhonnyjr2003@hotmail.com>
2021-06-18 00:51:15 -0400

    using material-ui data grid

--- src/Components/Display.jsx
+++ src/Components/Display.jsx
@@ -1,53 +1,96 @@
-import { Paper } from '@material-ui/core';
 import React from 'react';
+import { Paper, Grid, Typography, makeStyles, } from '@material-ui/core';
 import { useEffect, useState } from 'react';
 import externalApi from '../services/externalApi'
-import { formatDate, formatNumber, Source } from './Const'
-import { makeStyles } from '@material-ui/core/styles';
-import BarChar from './BarChar/Barchar';
+import { formatDate, formatNumber } from './Const'
+import { DataGrid, } from '@material-ui/data-grid';
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        maxWidth:'70vw'
-    }
+    dataGrid:{
+        height: 300,
+        width: '100%',
+    },
 }));
+const columns = [
+    {
+        fiel: "id",
+        hide: true
+    },
+    {
+        field: "name",
+        headerName: "Provedor",
+        
+    },
+    {
+        field: "price",
+        headerName: "Precio",
+        valueFormatter: (params) => formatNumber(params.value),
+        
+    },
+    {
+        field: "last_update",
+        headerName: "Ultima Actualización",
+        valueFormatter: (params) => params.value.split(' ')[0],
+        
+    }
+]
 
 function Display() {
-    const [data, setData] = useState({sources:[]})
-    const [today,setToday] = useState(formatDate(Date()))
-    const classes = useStyles()
+    const [data, setData] = useState({sources:[]});
+    const [rows, setRows] = useState([]);
+    const classes = useStyles();
 
+    const loadContent = async () => {
+        const quotes = await externalApi.get();
+        setData(quotes.data);
+        let row = []
+        quotes.data.sources.forEach((source, index) =>{
+            row.push({
+                id: index,
+                name: source.name,
+                price: source.quote,
+                last_update: source.last_retrieved
+            });
+        });
+        setRows(row)
+        console.log(quotes.data);
+    };
     useEffect(() => {
         //the API is not mine, if you want to check the source is
         //https://api.exchangedyn.com/free/quotes/usdves
-        externalApi.get()
-        .then((data)=> {
-            let newData = {average:data.data.average,sources:data.data.sources}
-            let sourceData = Object.create(Source)
-            newData.sources.map((source)=>{
-                sourceData['' +source.name] = source.quote
-            })
-            sourceData.date = today
-            console.log(sourceData)
-            newData.sources = [sourceData]
-            /*if(newData.sources.length == 1){
-                newData.sources.push(sourceData)
-            }*/
-            setData(newData)
-        });
+        loadContent();
     }, [])
 
-
-    return(
         //for now this will be the only option available
         //i have planed a few more, like getting prices from a range of dates
         //using my own API that still in development but with also be open source
+
+    return(
         <>
-            <Paper className={classes.root} >
-                <p>Fecha: {today}</p>
-                <p>Promedio: {formatNumber(data.average)}</p>
+            <Paper >
+                <Grid container justify='center'>
+                    <Grid container justify='center'>
+                        <Typography
+                        variant='h5'>
+                            Fecha: {formatDate(Date())}
+                        </Typography>
+                    </Grid>
+                    <Grid container justify='center'>
+                        <Typography
+                        variant='h5'>
+                            Promedio: {formatNumber(data.average)}
+                        </Typography>
+                    </Grid>
+                </Grid>
             </Paper>
-            <BarChar sources={data.sources} />
+            <Grid container justify='center'>
+                <DataGrid 
+                autoHeight
+                columns={columns}
+                rows={rows}
+                className={classes.dataGrid}  
+                />
+            </Grid>
         </>
     );
 }
