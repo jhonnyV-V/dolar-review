fb968df9388feb8f4cb27846348b8c97710dcfd1
Jhonny Varela <jhonnyjr2003@hotmail.com>
2021-07-25 01:02:25 -0400

    adding the service worker in order to activate it in the future

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
+import * as serviceWorker from './serviceWorker';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import { CssBaseline } from '@material-ui/core';
 
@@ -23,4 +24,5 @@ ReactDOM.render(<ThemeProvider theme={theme} >
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
