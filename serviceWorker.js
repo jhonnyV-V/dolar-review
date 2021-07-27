f31ad69e17f7783c3eac448fd27775b5dd1eaef5
Jhonny Varela <jhonnyjr2003@hotmail.com>
2021-07-27 16:14:41 -0400

    Updates

---
+++ serviceWorker.js
@@ -0,0 +1,26 @@
+let CACHE_NAME = 'dolar-review-cache-v1';
+const urlsToCache = [
+	'/',
+	'/index.html',
+	'/dolar-review',
+	'/dolar-review/calculator'
+];
+self.addEventListener('install', function(event) {
+	// Perform install steps
+	event.waitUntil(
+		caches.open(CACHE_NAME).then(function(cache) {
+			console.log('Opened cache');
+			return cache.addAll(urlsToCache);
+		})
+	);
+});
+
+self.addEventListener('fetch', function(event) {
+	event.respondWith(caches.match(event.request).then(function(response) {
+		if (response) {
+			return response;
+		}
+			return fetch(event.request);
+		})
+	);
+});
