c7af1413fcf927bb2a520f5d8438fa080380b1b9
Jhonny Varela <jhonnyjr2003@hotmail.com>
2021-07-27 22:39:40 -0400

    using workbox

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
-import * as serviceWorker from './serviceWorker';
-import { createMuiTheme, ThemeProvider, responsiveFontSizes,CssBaseline } from '@material-ui/core';
+import * as serviceWorker from './serviceWorkerRegistration';
+import { createMuiTheme, ThemeProvider, responsiveFontSizes, CssBaseline } from '@material-ui/core';
 
 const theme = responsiveFontSizes(
     createMuiTheme({
